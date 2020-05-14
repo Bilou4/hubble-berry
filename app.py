8db640f52026d4664646f9341f54b24502bd553c
Bilou4 <4quatre4quatre@gmail.com>
2020-05-14 17:06:40 +0200

    add a login route

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
 from flask import Flask
-app = Flask(__name__)
+from flask import render_template
+from config import Config
+from forms import LoginForm
 
+PROJECT_NAME = 'Hubble-Berry'
+
+app = Flask(__name__)
+app.config.from_object(Config)
+# login_manager = LoginManager()
+# login_manager.init_app(app)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    user = {'username': 'Bilou'}
+    return render_template('index.html', title=PROJECT_NAME, user=user)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', form=form)
+
 
 if __name__ == '__main__':
     app.run(host='localhost', port=8000, debug=True)
