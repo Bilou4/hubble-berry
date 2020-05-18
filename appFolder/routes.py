9e460ae56a2903d4f1108a5d6f01494362a25e89
Bilou4 <4quatre4quatre@gmail.com>
2020-05-18 12:02:46 +0200

    register form

--- appFolder/routes.py
+++ appFolder/routes.py
@@ -1,6 +1,6 @@
-from appFolder import app
+from appFolder import app, db
 from appFolder.models import User
-from appFolder.forms import LoginForm
+from appFolder.forms import LoginForm, RegistrationForm
 
 from flask import render_template, flash, redirect,\
      url_for, request
@@ -38,3 +38,17 @@ def login():
             next_page = url_for('index')
         return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
