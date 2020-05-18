2f8359498aec1d41d9a249a6056ed045292c9253
Bilou4 <4quatre4quatre@gmail.com>
2020-05-18 17:15:40 +0200

    new label for loginForm

--- appFolder/forms.py
+++ appFolder/forms.py
@@ -5,10 +5,10 @@ from wtforms.validators import DataRequired, ValidationError, Email, EqualTo
 from appFolder.models import User
 
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
+    username = StringField('Who are you?', validators=[DataRequired()])
+    password = PasswordField('What is your password?', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
-    submit = SubmitField('Sign In')
+    submit = SubmitField('Go!')
 
 
 
