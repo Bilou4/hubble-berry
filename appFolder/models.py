3499a6af2a12c4816b03cbc3cd8f7f7af63e0935
Bilou4 <4quatre4quatre@gmail.com>
2020-06-16 21:15:51 +0200

    changed model to use role

--- appFolder/models.py
+++ appFolder/models.py
@@ -10,6 +10,9 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    # Relationships
+    roles = db.relationship('Role', secondary='user_roles',
+                backref=db.backref('blabla', lazy=True))
 
     def __repr__(self):
         return '<User {}>'.format(self.username)    
@@ -20,7 +23,23 @@ class User(UserMixin, db.Model):
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
 
+    def set_default_role(self):
+        self.roles.append(UserRoles(user_id=self.id, role_id=2)) #1=admin, 2=user
 
+# Define the Role data model
+class Role(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(50), unique=True)
+
+# Define the UserRoles data model
+class UserRoles(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    user_id = db.Column(db.Integer(), db.ForeignKey('user.id', ondelete='CASCADE'))
+    role_id = db.Column(db.Integer(), db.ForeignKey('role.id', ondelete='CASCADE'))
+
+
+
+# The @login_manager.user_loader piece tells Flask-login how to load users given an id
 @login_manager.user_loader
 def load_user(id):
     return User.query.get(int(id))
