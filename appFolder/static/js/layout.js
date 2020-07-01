b317249b6af53248b070a4614adb453b9c0d14c5
Bilou4 <4quatre4quatre@gmail.com>
2020-07-01 19:36:34 +0200

    change night/day mode when click on the button

---
+++ appFolder/static/js/layout.js
@@ -0,0 +1,54 @@
+function switchOnNightMode(){
+    $('.container').css('color', '#e57373'); 
+    $('.button').css({
+        'background-color': '#d32f2f',
+        'color': '#e57373'
+    });
+    $('.messageError').css('color', '#e57373');
+    $('footer').css('color','#e57373');
+    $('input').css({
+        'background-color': 'black',
+        'color': '#e57373'
+    });
+    $('select').css({
+        'background-color': 'black',
+        'color': '#e57373'
+    });
+    $('.tab button').css('background-color', '#e57373'); 
+    // $('.tab button:hover').css('background-color', '#ddd'); 
+    $('.tab button.active').css('background-color', '#d32f2f');
+}
+
+function switchOffNightMode(){
+    $('.container').css('color', 'white'); 
+    $('.button').css({
+        'background-color': 'blueviolet',
+        'color': 'white'
+    });
+    $('.messageError').css('color', 'white');
+    $('footer').css('color','white');
+    $('input').css({
+        'background-color': 'black',
+        'color': 'white'
+    });
+    $('select').css({
+        'background-color': 'black',
+        'color': 'white'
+    });
+    $('.tab button').css('background-color', '#f1f1f1'); 
+    $('.tab button.active').css('background-color', '#ccc'); 
+
+}
+
+$(document).ready(function(){
+
+    // ############ Night Mode ############
+    $('#night_mode').change( function() {
+		if(this.checked){
+			switchOnNightMode();
+        }
+		else{
+            switchOffNightMode();
+		}
+    });
+});
