0567c662ad6eb17f984de5e6fd19bf2403b88244
Bilou4 <4quatre4quatre@gmail.com>
2020-07-03 19:00:51 +0200

    gestion de 2 langages

--- appFolder/__init__.py
+++ appFolder/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from flask import request
 
 from appFolder.config import Config
 
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_babel import Babel
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -13,6 +15,10 @@ migrate = Migrate(app, db)
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login' # redirige l'user vers la page 'login' si non connecté
-
+babel = Babel(app)
 
 from appFolder import routes, models
+
+@babel.localeselector
+def get_locale():
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
