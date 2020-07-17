dfe102e4803a1b805ba3c8554154bc25ceee92bf
Bilou4 <4quatre4quatre@gmail.com>
2020-07-17 17:29:49 +0200

    create a logger for the app

--- appFolder/__init__.py
+++ appFolder/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask import request
 
-from appFolder.config import Config
+from appFolder.config import Config, init_logger
 
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -16,7 +16,7 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login' # redirige l'user vers la page 'login' si non connecté
 babel = Babel(app)
-
+logger = init_logger(__name__, True)
 from appFolder import routes, models
 
 @babel.localeselector
