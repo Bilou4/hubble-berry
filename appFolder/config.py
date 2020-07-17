f0d94ff96e1998f42d77dcc4eb0ce06a4087513f
Bilou4 <4quatre4quatre@gmail.com>
2020-07-17 17:29:20 +0200

    created init_logger function

--- appFolder/config.py
+++ appFolder/config.py
@@ -1,5 +1,8 @@
 import os
 
+import logging
+import colorlog
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
@@ -8,3 +11,43 @@ class Config(object):
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     LANGUAGES = ['en', 'fr']
+
+
+def init_logger(dunder_name, testing_mode):
+    log_format = ('[%(levelname)s] - %(asctime)s - %(message)s')
+    bold_seq = '\033[1m'
+    colorlog_format = (
+        f'{bold_seq} '
+        '%(log_color)s '
+        f'{log_format}'
+    )
+    colorlog.basicConfig(format=colorlog_format)
+    logger = logging.getLogger(dunder_name)
+
+    if testing_mode:
+        logger.setLevel(logging.DEBUG)
+    else:
+        logger.setLevel(logging.INFO)
+
+    # Output full log
+    # fh = logging.FileHandler('app.log')
+    # fh.setLevel(logging.DEBUG)
+    # formatter = logging.Formatter(log_format)
+    # fh.setFormatter(formatter)
+    # logger.addHandler(fh)
+
+    # Output warning log
+    # fh = logging.FileHandler('app.warning.log')
+    # fh.setLevel(logging.WARNING)
+    # formatter = logging.Formatter(log_format)
+    # fh.setFormatter(formatter)
+    # logger.addHandler(fh)
+
+    # Output error log
+    # fh = logging.FileHandler('app.error.log')
+    # fh.setLevel(logging.ERROR)
+    # formatter = logging.Formatter(log_format)
+    # fh.setFormatter(formatter)
+    # logger.addHandler(fh)
+
+    return logger
