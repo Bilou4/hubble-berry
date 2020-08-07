474bfbc016f3a466adc4ca956c71861c9e05bc5b
Bilou4 <4quatre4quatre@gmail.com>
2020-08-07 10:55:23 +0200

    passing arguments to make_timelapse function

--- pylapse.py
+++ pylapse.py
@@ -2,25 +2,25 @@ import cv2
 import os
 import argparse
 
-def make_timelapse():
+def make_timelapse(directory, fps):
     print('timelapse')
-    # l = []
-    # if os.path.exists(path=directory):
-    #     l = os.listdir(directory)
-    #     l = sorted(l)
-    #     height , width , layers =  cv2.imread(directory + l[0]).shape
-    #     video = cv2.VideoWriter('video.avi', cv2.VideoWriter_fourcc(*"MJPG"),
-    #                             fps, (width, height))
-    #     for img_path in l:
-    #         video.write(cv2.imread(directory + img_path))
-    #     cv2.destroyAllWindows()
-    #     video.release()
-    #     print('video done')
-    # else:
-    #     print('cannot find the directory - ' + directory)
+    l = []
+    if os.path.exists(path=directory):
+        l = os.listdir(directory)
+        l = sorted(l)
+        height , width , layers =  cv2.imread(directory + l[0]).shape
+        video = cv2.VideoWriter('video.avi', cv2.VideoWriter_fourcc(*"MJPG"),
+                                fps, (width, height))
+        for img_path in l:
+            video.write(cv2.imread(directory + img_path))
+        cv2.destroyAllWindows()
+        video.release()
+        print('video done')
+    else:
+        print('cannot find the directory - ' + directory)
 
 def make_star_trail():
-    print('star trail')
+    print('star trail not implemented yet')
     
 parser = argparse.ArgumentParser()
 parser.add_argument("-d", "--directory", help="the directory containing photos for the timelapse", required=True)
@@ -34,15 +34,11 @@ if(args.directory is not None and args.time is not None and args.action is not N
     directory = args.directory
     time_one_im = args.time
     fps = 1/time_one_im # may need a cast to int
-    print(directory)
-    print(time_one_im)
-    print(fps)
-    print(args.action)
     if (args.action == 'timelapse'):
-        make_timelapse()
+        make_timelapse(directory, fps)
     elif (args.action == 'startrail'):
         make_star_trail()
     else:
-        make_timelapse()
+        make_timelapse(directory, fps)
         make_star_trail()
 
