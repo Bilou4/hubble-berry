3c8f3700d5e6a55bbd1f9d19c2662029aed4e396
Bilou4 <4quatre4quatre@gmail.com>
2020-08-13 09:39:27 +0200

    first lines for ssh connections & scp

--- download.py
+++ download.py
@@ -2,6 +2,8 @@
 # -*- coding: utf-8 -*-
 import argparse
 from enum import Enum
+from paramiko import SSHClient
+from scp import SCPClient
 
 class Action(Enum):
     COPY_PICTURES = 1
@@ -12,8 +14,9 @@ class Action(Enum):
     MOVE_VIDEOS = 32
 
 
-def cp_pictures():
+def cp_pictures(scp):
     print("cp_pictures")
+    scp.get('/home/pi/Documents/hubble-berry/appFolder/static/camera/pictures/*.jpg', '/home/bilou/Téléchargements/pictures/')
 
 def cp_timelapse():
     print("cp_timelapse")
@@ -71,8 +74,18 @@ if args.move:
 if not action_performed:
     parser.error("You need to choose an action to perform, either copy or move files.")
 else:
+    ssh = SSHClient()
+    ssh.load_system_host_keys()
+    ssh.connect(hostname='10.3.141.1', 
+            port = '22',
+            username='pi',
+            password='changeMe',
+            pkey='load_key_if_relevant')
+    # SCPCLient takes a paramiko transport as its only argument
+    scp = SCPClient(ssh.get_transport())
+
     if action & Action.COPY_PICTURES.value:
-        cp_pictures()
+        cp_pictures(scp)
     if action & Action.COPY_TIMELAPSES.value:
         cp_timelapse()
     if action & Action.COPY_VIDEOS.value:
@@ -83,3 +96,5 @@ else:
         move_timelapse()
     if action & Action.MOVE_VIDEOS.value:
         move_videos()
+
+    scp.close()
