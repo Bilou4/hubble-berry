892f4a6f863a484315d2bf89af7b89f97bca4e8a
Bilou4 <4quatre4quatre@gmail.com>
2020-09-01 18:20:16 +0200

    the script can change CR2 file to jpg

--- scripts/fromRawToJPG.py
+++ scripts/fromRawToJPG.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import argparse
 import os
-import tqdm
+from tqdm import tqdm
 from shutil import which
 
 
@@ -10,10 +11,13 @@ def is_tool(name):
     """Check whether `name` is on PATH and marked as executable."""
     return which(name) is not None
 
-
+def escape_chars(string):
+    string = string.replace(' ', '\\ ')
+    string = string.replace('(', '\(')
+    string = string.replace(')', '\)')
+    return string
 
 parser = argparse.ArgumentParser()
-
 parser.add_argument("-i", "--input", help="the input directory containing photos")
 parser.add_argument("-o", "--output", help="the output directory containing tconverted photos")
 
@@ -23,9 +27,14 @@ if args.input is None or args.output is None:
     parser.error("To convert your photos, you need to define an input directory and an output directory")
 input_directory = args.input
 output_directory = args.output
-print(input_directory,output_directory)
 
-#os.system("ufraw-batch *.CR2 --silent --out-type=jpeg --compression=95 --wb=camera")
-# taux compression + 
-# input + list dir + exécuter commande fichier par fichier + tqdm
-# 
+if is_tool("ufraw-batch"):
+    l = []
+    if os.path.exists(path=input_directory) and os.path.exists(path=output_directory):
+        l = os.listdir(input_directory)
+        for raw_path in tqdm(l):
+            input_full_path = input_directory + raw_path
+            input_full_path = escape_chars(input_full_path)
+            os.system("ufraw-batch {} --out-path {} --silent --out-type=jpeg --compression=95 --wb=camera".format(input_full_path, output_directory))
+
+# taux compression 
