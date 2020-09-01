8ebbc3735f578ffeec7ca8892e2b106fa1aecd11
Bilou4 <4quatre4quatre@gmail.com>
2020-09-01 18:30:02 +0200

    compression rate can be modified in the command line

--- scripts/fromRawToJPG.py
+++ scripts/fromRawToJPG.py
@@ -20,11 +20,15 @@ def escape_chars(string):
 parser = argparse.ArgumentParser()
 parser.add_argument("-i", "--input", help="the input directory containing photos")
 parser.add_argument("-o", "--output", help="the output directory containing tconverted photos")
+parser.add_argument("-c", "--compression", type=int, default=85, help="the compression rate of the jpg image (default: 85)")
 
 args = parser.parse_args()
 
 if args.input is None or args.output is None:
     parser.error("To convert your photos, you need to define an input directory and an output directory")
+if args.compression < 0 or args.compression > 100:
+    parser.error("The compression rate must be between 0 and 100. Higher is the value, better is the quality.")
+
 input_directory = args.input
 output_directory = args.output
 
@@ -35,6 +39,6 @@ if is_tool("ufraw-batch"):
         for raw_path in tqdm(l):
             input_full_path = input_directory + raw_path
             input_full_path = escape_chars(input_full_path)
-            os.system("ufraw-batch {} --out-path {} --silent --out-type=jpeg --compression=95 --wb=camera".format(input_full_path, output_directory))
+            os.system("ufraw-batch {} --out-path {} --silent --out-type=jpeg --compression={} --wb=camera"
+                    .format(input_full_path, output_directory, str(args.compression)))
 
-# taux compression 
