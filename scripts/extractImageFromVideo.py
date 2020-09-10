4a70d8b892f049df4876bb7e682a1b9a60c86302
Bilou4 <4quatre4quatre@gmail.com>
2020-09-10 10:35:37 +0200

    there are arguments in extractImageFromVideo

--- scripts/extractImageFromVideo.py
+++ scripts/extractImageFromVideo.py
@@ -1,12 +1,44 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
 import cv2
-cap = cv2.VideoCapture('/path/to/video')
+import os
+import argparse
+import mimetypes
+
+def is_a_video(input_file):
+    mimetypes.init()
+    mimestart = mimetypes.guess_type(input_file)[0]
+    if mimestart != None:
+        mimestart = mimestart.split('/')[0]
+        if mimestart == 'video':
+            return True
+    return False
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-i", "--input", help="the input video")
+parser.add_argument("-o", "--output", help="the output folder that will contain the images from the video")
+
+args = parser.parse_args()
+
+if args.input is None or args.output is None:
+    parser.error("To extract images from a video, you need to specify an input video & an output directory")
+input_file = args.input
+output_directory = args.output
+
+if is_a_video(input_file) and os.path.isdir(output_directory):
+    if output_directory[-1] != '/':
+        output_directory = output_directory + '/'
+else:
+    raise Exception("Either the input is not a video file or the output is not a directory")
+
+cap = cv2.VideoCapture(input_file)
 count = 0
 while cap.isOpened():
     ret,frame = cap.read()
-    cv2.imshow('window-name',frame)
-    cv2.imwrite("/path/to/output/frame%d.jpg" % count, frame)
+    #cv2.imshow('window-name',frame)
+    cv2.imwrite(output_directory + 'frame%d.jpg' % count, frame)
     count = count + 1
     if cv2.waitKey(10) & 0xFF == ord('q'): # press q to exit the program
         break
